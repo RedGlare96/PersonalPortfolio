af8394e0e78629fab1a0ffa4c09809b4e12d490b
RedGlare96 <ashwinpranesh11@gmail.com>
2020-11-04 16:48:44 +0530

    Made the repo 'clone-ready'

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from configparser import ConfigParser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(&nb1(oefaba&f4wm%rq)xw&brn-*e%@1o3tfv90flv5memnz9'
+config = ConfigParser(interpolation=None)
+config.read('key.ini')
+SECRET_KEY = config['key']['keystring']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
