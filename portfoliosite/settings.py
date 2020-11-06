87eae4a06ef5dcb7bdd10c293959a90ff701e5fc
RedGlare96 <ashwinpranesh11@gmail.com>
2020-11-06 17:17:24 +0530

    Additional visual changes

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config['key']['keystring']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['redglare96.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
